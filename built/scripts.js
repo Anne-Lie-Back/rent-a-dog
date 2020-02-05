441647287699ca64487b71b45cd025c5774cd529
Anne-Lie Bäck <56996804+Anne-Lie-Back@users.noreply.github.com>
2020-02-05 13:04:51 +0100

    burgermenu 1.0

--- built/scripts.js
+++ built/scripts.js
@@ -3,6 +3,25 @@ function init() {
     var hero = document.querySelector('.hero');
     var slider = document.querySelector('.slider');
     var logo = document.querySelector('logo');
-    var burger = document.querySelector('.burger');
     var headline = document.querySelector('.headline');
+    handleNavMenu();
+}
+function handleNavMenu() {
+    var navSlide = function () {
+        var navLinks = document.querySelectorAll('.navLinks li');
+        var burger = document.querySelector('.burger');
+        var nav = document.querySelector('.navLinks');
+        burger.addEventListener('click', function () {
+            nav.classList.toggle('nav-active');
+            navLinks.forEach(function (link, index) {
+                if (link.style.animation) {
+                    link.style.animation = '';
+                }
+                else {
+                    link.style.animation = "navLinkFade 0.5s ease forwards " + (index / 7 + 0.5) + "s";
+                }
+            });
+        });
+    };
+    navSlide();
 }
--- src/scripts.ts
+++ src/scripts.ts
@@ -1,11 +1,32 @@
 window.addEventListener('load', init);
 
-
 function init(){
-    const hero = document.querySelector('.hero');
-    const slider = document.querySelector('.slider');
-    const logo = document.querySelector('logo');
-    const burger = document.querySelector('.burger');
-    const headline = document.querySelector('.headline');
+    const hero:Element = document.querySelector('.hero');
+    const slider:Element = document.querySelector('.slider');
+    const logo:Element = document.querySelector('logo');
+    const headline:Element = document.querySelector('.headline');
+    
+    handleNavMenu();
 }
 
+function handleNavMenu(){
+    const navSlide = () => {
+        const navLinks = document.querySelectorAll('.navLinks li');
+        const burger:Element = document.querySelector('.burger');
+        const nav:Element = document.querySelector('.navLinks');
+        
+        burger.addEventListener('click', () => {
+            nav.classList.toggle('nav-active');
+
+            navLinks.forEach((link:any, index)=>{
+                if(link.style.animation){
+                    link.style.animation = '';
+                }
+                else{
+                    link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+                }
+            })
+        }); 
+    }
+    navSlide();
+}
